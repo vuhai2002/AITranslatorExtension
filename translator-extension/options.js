1935c6eb0f8813f3ff8f16e18513e7607db7e98a
Vu Van Hai <HaiVV.SE@gmail.com>
2025-03-04 16:26:33 +0700

    delete option page

--- translator-extension/options.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("saveApiKey").addEventListener("click", () => {
-    const key = document.getElementById("apiKey").value;
-    chrome.storage.sync.set({ apiKey: key }, () => {
-        alert("Đã lưu API Key!");
-    });
-});
